79a81b5791f97a61affd237430bbff4532fb33b5
henrikhelsingborg <henrik.wilhelmsson@helsingborg.se>
2015-07-16 10:58:45 +0200

    first commit

---
+++ nordiskinfra_twentytwelve/footer.php
@@ -0,0 +1,43 @@
+﻿<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'twentytwelve_credits' ); ?>
+			
+		</div><!-- .site-info -->
+		<div class="footertext">
+			<div class="footerheading">
+
+
+			</div>
+			<div class="leftfooter">
+
+
+			</div>
+			<div class="rightfooter">
+
+
+			</div>
+			<div class="middlefooter">
+
+			</div>
+
+		</div>
+
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
